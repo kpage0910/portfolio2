4face570ec636b885f1a07ecbc1e54b9c963a7cd
kpage0910 <kpage6984@gmail.com>
2025-07-25 03:58:45 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Toggle sidebar on mobile
+const toggleBtn = document.getElementById("menu-toggle");
+const sidebar = document.getElementById("sidebar");
+const closeBtn = document.getElementById("close-sidebar");
+const overlay = document.getElementById("overlay");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  toggleBtn.style.display = "none";
+});
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  toggleBtn.style.display = "block";
+});
+
+// Close sidebar if user clicks on the dark overlay
+overlay.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+  toggleBtn.style.display = "block";
+});
